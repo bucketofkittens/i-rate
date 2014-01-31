e3fc8e3169d1486a958dcfdc3a2fae31b3a1dc20
lyykfi <lyykfi@gmail.com>
2014-01-31 16:13:07 +0400

    правка

---
+++ js/libs/utils.js
@@ -0,0 +1,62 @@
+
+Array.prototype.shuffle = function(b) {
+    var i = this.length, j, t;
+    while(i) {
+        j = Math.floor( ( i-- ) * Math.random() );
+        t = b && typeof this[i].shuffle!=='undefined' ? this[i].shuffle() : this[i];
+        this[i] = this[j];
+        this[j] = t;
+    }
+
+    return this;
+};
+
+Array.prototype.remove = function(from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+      this.length = from < 0 ? this.length + from : from;
+      return this.push.apply(this, rest);
+};
+
+function degToRad (deg) { return deg / 180 * Math.PI; }
+function radToDeg (rad) { return rad / Math.PI * 180; }
+
+function randomRange(l,h){
+  var range = (h-l);
+  var random = Math.floor(Math.random()*range);
+  if (random === 0){random+=1;}
+  return l+random;
+}
+
+function getCookie(name) {
+	var matches = document.cookie.match(new RegExp(
+	  "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+	))
+	return matches ? decodeURIComponent(matches[1]) : undefined 
+}
+
+// уcтанавливает cookie
+function setCookie(name, value, props) {
+	props = props || {}
+	var exp = props.expires
+	if (typeof exp == "number" && exp) {
+		var d = new Date()
+		d.setTime(d.getTime() + exp*1000)
+		exp = props.expires = d
+	}
+	if(exp && exp.toUTCString) { props.expires = exp.toUTCString() }
+
+	value = encodeURIComponent(value)
+	var updatedCookie = name + "=" + value
+	for(var propName in props){
+		updatedCookie += "; " + propName
+		var propValue = props[propName]
+		if(propValue !== true){ updatedCookie += "=" + propValue }
+	}
+	document.cookie = updatedCookie
+
+}
+
+// удаляет cookie
+function deleteCookie(name) {
+	setCookie(name, null, { expires: -1 })
+}
