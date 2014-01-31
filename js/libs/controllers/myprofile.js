745d29b0c8be41c0be9645631cf9200430a9599e
lyykfi <lyykfi@gmail.com>
2014-01-31 17:59:18 +0400

    правки своего профиля

--- js/libs/controllers/myprofile.js
+++ js/libs/controllers/myprofile.js
@@ -1,35 +1,38 @@
+// контроллер вкладок своего профиля
 function MyProfileController($scope) {
+	// показываем плашку или нет
 	$scope.showProfile = false;
 
-	$scope.nav = [
+	// пункты меню навигации
+	$scope.navItems = [
 		{
-			name: 'Profile',
-			index: 1
+			name: 'Profile'
 		},
 		{
-			name: 'Dashboard',
-			index: 2
+			name: 'Dashboard'
 		},
 		{
-			name: 'Settings',
-			index: 3
+			name: 'Settings'
 		}
 	];
 
+	// список индексов
 	$scope.indexes = {
-		PROFILE: 1,
-		DASHBOARD: 2,
-		SETTINGS: 3
+		PROFILE: 'Profile',
+		DASHBOARD: 'Dashboard',
+		SETTINGS: 'Settings'
 	}
 
-	$scope.currentNav = $scope.nav[0];
+	// текущий выбранный элемент
+	$scope.currentNav = $scope.navItems[0];
 
+	// выбираем другое состояние
 	$scope.changeState = function(state) {
 		$scope.currentNav = state;
 	}
 
+	// открываем эту плашку
 	$scope.$on('openProfile', function(event, message) {
         $scope.showProfile = true;
-        console.log($scope.showProfile);
     });
 }
---
+++ js/libs/controllers/myprofile_settings.js
@@ -0,0 +1,3 @@
+function MyProfileSettingsController($scope) {
+	
+}
