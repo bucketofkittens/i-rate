e25b2fa8df4afaedf72ebacd8f263534b920ecb5
lyykfi <lyykfi@gmail.com>
2014-02-03 14:59:37 +0400

    dfg

--- js/libs/controllers/myprofile_profile.js
+++ js/libs/controllers/myprofile_profile.js
@@ -1,3 +1,5 @@
 function MyProfileProfileController($scope) {
-	
+	$scope.goalClick = function($event, needItem, goalItem, needs) {
+		$scope.openCriteriumList($event, needItem, goalItem, needs);	
+	}
 }
