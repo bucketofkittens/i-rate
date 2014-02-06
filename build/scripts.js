d70b1e384f44e44ba94128338d3697db2142f50e
lyykfi <lyykfi@gmail.com>
2014-02-06 18:07:34 +0400

    правки по signup

--- build/scripts.js
+++ build/scripts.js
@@ -1814,9 +1814,10 @@ function FollowCaruselController($scope) {
 
 
 
-function ConfirmController($scope, ConfirmSignup, $routeParams, $rootScope) {
+function ConfirmController($scope, ConfirmSignup, $routeParams, $rootScope, $location) {
     ConfirmSignup.test({hash: $routeParams.hash}, {}, function(data) {
         $rootScope.$broadcast('openModal', {name: "confirm-success"});
+        $location.path("/");
     });
 }
 pgrModule.directive('errSrc', function() {
--- js/libs/controllers.js
+++ js/libs/controllers.js
@@ -229,8 +229,9 @@ function FollowCaruselController($scope) {
 
 
 
-function ConfirmController($scope, ConfirmSignup, $routeParams, $rootScope) {
+function ConfirmController($scope, ConfirmSignup, $routeParams, $rootScope, $location) {
     ConfirmSignup.test({hash: $routeParams.hash}, {}, function(data) {
         $rootScope.$broadcast('openModal', {name: "confirm-success"});
+        $location.path("/");
     });
 }
