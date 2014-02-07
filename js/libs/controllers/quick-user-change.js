283e22c36d5424b2ffb12e4514d40b366ed6d975
lyykfi <lyykfi@gmail.com>
2014-02-07 16:26:35 +0400

    поправил что-то )

--- js/libs/controllers/quick-user-change.js
+++ js/libs/controllers/quick-user-change.js
@@ -2,10 +2,14 @@
  *  плашка быстрой смены пользователей
  */
 function QuickUserChangeCtrl($scope, UserService) {
-	// переход на другого пользователя
-    $scope.onMoveUserClick = function($event, nextUser) {
-    	UserService.setAuthData(nextUser);
 
+	// переход на другого пользователя
+    $scope.onMoveUserClick = function($event) {
+        UserService.getById($scope.nextUser, $scope.onSigninSuccessCallback_);
+    }
+
+    $scope.onSigninSuccessCallback_ = function(data) {
+        UserService.setAuthData(data);
         window.location.reload();
     }
 }
