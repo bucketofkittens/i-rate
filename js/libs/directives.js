252ef58a1eb83cbc8b84391592848f912426524a
lyykfi <lyykfi@gmail.com>
2014-02-10 19:43:02 +0400

    поправил повторение пароля

--- js/libs/directives.js
+++ js/libs/directives.js
@@ -616,3 +616,37 @@ pgrModule.directive('mydash', function(User) {
     }
   }
 })
+
+pgrModule.directive('equal', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            scope.$watch(attrs.equal, function (newValue) {
+                if (ctrl && ctrl.$modelValue) {
+                    if (newValue === ctrl.$modelValue) {
+                        ctrl.$setValidity('equal', true);
+                        // return newValue;
+                    } else {
+                        ctrl.$setValidity('equal', false);
+                        // return undefined;
+                    }
+                }
+            });
+            
+            ctrl.$parsers.unshift(function(viewValue) {
+                if (viewValue) {
+                    if (viewValue === scope.$eval(attrs.equal)) {
+                        ctrl.$setValidity('equal', true);
+                        return viewValue;
+                    } else {
+                        ctrl.$setValidity('equal', false);
+                        return undefined;
+                    }
+                } else {
+                    ctrl.$setValidity('equal', true);
+                    return viewValue;
+                }
+            });
+        }
+    };
+});
