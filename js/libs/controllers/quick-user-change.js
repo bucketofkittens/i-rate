e64225c4b4272226f91a18cd4b168b9d309cbde9
lyykfi <lyykfi@gmail.com>
2014-02-16 16:46:28 +0400

    в списке выводим всех пользователей

--- js/libs/controllers/quick-user-change.js
+++ js/libs/controllers/quick-user-change.js
@@ -1,10 +1,20 @@
 /**
  *  плашка быстрой смены пользователей
  */
-function QuickUserChangeCtrl($scope, UserService) {
+function QuickUserChangeCtrl($scope, UserService, User) {
+	$scope.nextUser = null;
+	$scope.usersList = [];
+
+	User.get_all({}, {}, function(data) {
+		angular.forEach(data, function(value, key){
+			value.title = value.name ? value.login + ", " + value.name : value.login;
+		});
+		$scope.usersList = data;
+	});
 
 	// переход на другого пользователя
     $scope.onMoveUserClick = function($event) {
+    	console.log($scope);
         UserService.getById($scope.nextUser, $scope.onSigninSuccessCallback_);
     }
 
