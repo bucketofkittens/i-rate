9ac7e99987b4a67aa6a05fb353a35132a82d5507
lyykfi <lyykfi@gmail.com>
2014-03-06 21:45:23 +0400

    dfg

--- js/libs/app.js
+++ js/libs/app.js
@@ -103,10 +103,12 @@ pgrModule.factory('httpRequestInterceptor', function() {
     		config.headers = {};
     	}
 
-    	config.headers['AUTH_TOKEN'] = token.split('"').join("");
-    	config.headers['REMOTE_USER'] = user.split('"').join("");
-    	
-      	return config;
+      if(user && token) {
+        config.headers['AUTH_TOKEN'] = token.split('"').join("");
+        config.headers['REMOTE_USER'] = user.split('"').join("");  
+      }
+      
+      return config;
     }
   };
 });
