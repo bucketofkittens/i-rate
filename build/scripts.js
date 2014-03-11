0736df340d14f735bce7c7ca9639e77c1fc950a7
lyykfi <lyykfi@gmail.com>
2014-03-11 20:35:58 +0400

    правки

--- build/scripts.js
+++ build/scripts.js
@@ -4617,68 +4617,6 @@ pgrModule.config(function(AnalyticsProvider) {
     AnalyticsProvider.setPageEvent('$stateChangeSuccess');
   }
 );
-'use strict';
-
-
-
-
-function LeaguesController($scope, Leagues, User) {
-    $scope.leagues = [];
-
-    /**
-    * Забираем запросом список лиг.
-    * @param  {[type]} data [description]
-    * @return {[type]}      [description]
-    */
-    Leagues.query({}, {}, function(data) {
-        data = data.reverse();
-
-        $scope.leagues = data;
-        angular.forEach($scope.leagues, function(value, key){
-           User.by_league({league_guid: value.sguid}, {}, function(v2, k2) {
-                v2 = v2.filter(function(value) {
-                    if(value.published == 1) {
-                        return value;
-                    }
-                });
-                value.users = v2;
-            })
-        });
-    })
-}
-
-
-
-
-
-
-
-
-
-function FollowCaruselController($scope) {
-    $scope.position = 0;
-    $scope.countFrend = 0;
-
-    $scope.onLeft = function() {
-        $scope.position -= 1;
-    }
-
-    $scope.onRight = function() {
-        $scope.position += 1;
-    }
-
-    $scope.$watch("frends", function (newVal, oldVal, scope) {
-        $scope.countFrend = newVal.length;
-    });
-}
-
-
-
-
-
-
-
-
 pgrModule.directive('errSrc', function() {
   return {
     link: function(scope, element, attrs) {
