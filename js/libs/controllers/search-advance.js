ac5872931092bd70110f56f53cff115e8cad2c69
lyykfi <lyykfi@gmail.com>
2014-03-11 16:53:29 +0400

    поправил расширенный поиск

--- js/libs/controllers/search-advance.js
+++ js/libs/controllers/search-advance.js
@@ -211,7 +211,9 @@ function SearchAdvanceController($scope, $location, $rootScope, User, Profession
             profession: {},
             country: {},
             city: {},
-            league: {}
+            league: {},
+            minScore: 0,
+            maxScore: 175000
         };
 
         $scope.shows = {
