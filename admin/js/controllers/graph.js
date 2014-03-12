27e20769cf6a8bfb4b1da5129204a585709217e6
lyykfi <lyykfi@gmail.com>
2014-03-12 18:31:04 +0400

    sdf

--- admin/js/controllers/graph.js
+++ admin/js/controllers/graph.js
@@ -1,5 +1,7 @@
 /** Контроллер графика */
 function GraphsController($scope, $rootScope, $route, $location, Leagues, User) {
+
+    $scope.users = [];
     /**
      * Сурово и беспощадно.
      * Надо будет переписать
@@ -30,7 +32,8 @@ function GraphsController($scope, $rootScope, $route, $location, Leagues, User) 
     */
     $scope.$watch('workspace.leagues', function (newVal, oldVal, scope) {
         angular.forEach($scope.workspace.leagues, function(value, key){
-            User.by_league({league_guid:value.sguid}, {}, function(v2, k2){
+            
+            /*User.by_league({league_guid:value.sguid}, {}, function(v2, k2){
                 v2.sort(function(a, b) {
                     if(a.points < b.points) return 1;
                     if(a.points > b.points) return -1;
@@ -44,8 +47,12 @@ function GraphsController($scope, $rootScope, $route, $location, Leagues, User) 
                     }
                 }
                 value.users = users;
-            })
+            })*/
         });
+    });
+
+    User.get_all({}, {}, function(datas) {
+        $scope.users = datas;
     });
 
     /**
@@ -53,13 +60,7 @@ function GraphsController($scope, $rootScope, $route, $location, Leagues, User) 
      * @param  {[type]} datas [description]
      * @return {[type]}       [description]
     */
-    User.get_all({}, {}, function(datas) {
-        $scope.looserUser = datas.filter(function(item) {
-            if(item.points == 0) {
-                return item;
-            }
-        });
-    });
+    
  
     $scope.openUser = function(userItem) {
         $rootScope.$broadcast('closeAllGoals');
