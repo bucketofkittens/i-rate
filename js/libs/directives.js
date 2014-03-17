9d1877731c3649636dc8a65ef836b17000cfd782
lyykfi <lyykfi@gmail.com>
2014-03-17 14:19:44 +0400

    поправил сравнение

--- js/libs/directives.js
+++ js/libs/directives.js
@@ -236,6 +236,8 @@ pgrModule.directive('comparator', function() {
       attrs.$observe('values', function(data) {
         if(data && data.length > 0) {
           var values = JSON.parse(data);
+
+          element.removeClass(classes.DOWN).removeClass(classes.UP).removeClass(classes.CENTER);
           
           if(values[usersName.USER1] && values[usersName.USER2]) {
             if(values[usersName.USER1] > values[usersName.USER2]) {
@@ -249,12 +251,13 @@ pgrModule.directive('comparator', function() {
             }
           } else {
             element.removeClass(classes.DOWN).removeClass(classes.UP).removeClass(classes.CENTER);
-                        /*if(!values[usersName.USER1] && values[usersName.USER2]) {
+
+            if(!values[usersName.USER1] && values[usersName.USER2]) {
               element.addClass(classes.UP);
             }
             if(!values[usersName.USER2] && values[usersName.USER1]) {
               element.addClass(classes.DOWN);
-            }*/
+            }
           }
         } else {
           element.removeClass(classes.DOWN).removeClass(classes.UP).removeClass(classes.CENTER);
