e5e26b121119dcb58780eb9ec490878e1934eb02
lyykfi <lyykfi@gmail.com>
2014-03-20 13:57:06 +0400

    egf

--- build/scripts.js
+++ build/scripts.js
@@ -7304,7 +7304,6 @@ function LeaguesController($scope, $location, $rootScope, User, LocationService,
     $scope.testLeague = function() {
         // определяем показываем ли мы панель или нет
         $scope.show = $location.search().leagues ? true : false;
-
         if($scope.show) {
             // забираем данные по лигам
             $rootScope.$broadcast('reloadLeagues');
@@ -9055,7 +9054,18 @@ function SearchAdvanceController($scope, $location, $rootScope, User, Profession
 
     // определяем показываем ли мы панель или нет
     $scope.showTest = function() {
-        $scope.show = $location.search().search  ? true : false;    
+        $scope.show = $location.search().search  ? true : false;
+
+        if($scope.show) {
+            // загружаем список лиг
+            $rootScope.$broadcast('reloadLeagues');
+
+            // загружаем список стран
+            $rootScope.$broadcast('countryLoad');
+
+            // загружаем список карьер
+            $rootScope.$broadcast('needsLoad');     
+        }
     }
 
     $scope.showTest();
@@ -9177,15 +9187,17 @@ function SearchAdvanceController($scope, $location, $rootScope, User, Profession
     });
 
     $scope.$watch("workspace.needs", function (newVal, oldVal, scope) {
-        var needs = JSON.parse(JSON.stringify($scope.workspace.needs));
+        if($scope.workspace.needs) {
+            var needs = JSON.parse(JSON.stringify($scope.workspace.needs));
         
-        angular.forEach(needs, function(need, key) {
-            angular.forEach(need.goals, function(goal, key) {
-                goal.show = true;
-                goal.needName = need.name;
-                $scope.topList.push(goal);
-            });
-        });
+            angular.forEach(needs, function(need, key) {
+                angular.forEach(need.goals, function(goal, key) {
+                    goal.show = true;
+                    goal.needName = need.name;
+                    $scope.topList.push(goal);
+                });
+            });    
+        }
     });
 
     // таймер отправки запроса на сервер
@@ -9539,14 +9551,6 @@ function SearchAdvanceController($scope, $location, $rootScope, User, Profession
         yearRange: '1900:-0'
     };
 
-    // загружаем список лиг
-    $rootScope.$broadcast('reloadLeagues');
-
-    // загружаем список стран
-    $rootScope.$broadcast('countryLoad');
-
-    // загружаем список карьер
-    $rootScope.$broadcast('needsLoad');
 
     $scope.careerServiceCallback_ = function(data) {
         angular.forEach(data, function(value, key) {
