3c4176107feb9fda17a64e7b584e5050b5caa976
lyykfi <lyykfi@gmail.com>
2014-03-24 12:47:44 +0400

    12345

--- js/libs/directives.js
+++ js/libs/directives.js
@@ -91,6 +91,29 @@ pgrModule.directive('textsizechange', function() {
   }
 });
 
+pgrModule.directive('sameAs', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      var otherInput = elm.inheritedData("$formController")[attrs.sameAs];
+
+      ctrl.$parsers.push(function(value) {
+          if(value === otherInput.$viewValue) {
+              ctrl.$setValidity("repeat", true);
+              return value;
+          }
+          console.log(ctrl);
+          ctrl.$setValidity("repeat", false);
+      });
+
+      otherInput.$parsers.push(function(value) {
+          ctrl.$setValidity("repeat", value === ctrl.$viewValue);
+          return value;
+      });
+    }
+  };
+});
+
 pgrModule.directive('positionGoal', function() {
   return {
     link: function(scope, element, attrs) {
