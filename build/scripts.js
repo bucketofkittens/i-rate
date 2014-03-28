10dbafbc9e9f128d5bceae3bb54a5126f12a7a77
Ubuntu <ubuntu@ip-10-234-59-30.us-west-2.compute.internal>
2014-03-28 13:22:24 +0000

    build

--- build/scripts.js
+++ build/scripts.js
@@ -6709,7 +6709,7 @@ function ChangePasswordController($scope, Sessions, User, $location, $rootScope,
                     $scope.userSguid = data.guid;
                     $scope.message = 1;
 
-                    LocationService.update("mail_hash", "true");
+                    LocationService.update("hash", "true");
                 });
             } else {
                 $scope.isEmailNotFound = true;
@@ -6720,7 +6720,7 @@ function ChangePasswordController($scope, Sessions, User, $location, $rootScope,
     $scope.onChangePasswordBegin = function() {
         var user = {
             "password": $scope.form.newPassword,
-            "reset_hash": $location.search().mail_hash
+            "reset_hash": $scope.form.code
         }
 
         $scope.dismath = false;
