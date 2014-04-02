91ff0f18fd46be8c8c8b82acdb7fc4b363dd598f
lyykfi <lyykfi@gmail.com>
2014-04-02 17:44:41 +0400

    fix

--- js/libs/directives.js
+++ js/libs/directives.js
@@ -353,27 +353,36 @@ pgrModule.directive('caruselPosition', function($window) {
   }
 })
 
+function module(number) { 
+  if (number < 0) { 
+  return number/-1; 
+  } else { 
+  return number; 
+  } 
+} 
+
 pgrModule.directive('showcrits', function($window) {
   return {
     link: function(scope, element, attrs) {
       var lastX;
       var lastY;
-      if(scope.phone) {
-        $(element).on("touchmove", function($event) {
-          if($(element).hasClass("show_crits")) {
+      //if(scope.phone) {
+        $(element).on("touchmove mousemove", function($event) {
+          //if($(element).hasClass("show_crits")) {
             //console.log($event.originalEvent);
             var currentX = $event.originalEvent.touches ? $event.originalEvent.touches[0].pageX : $event.pageX;
             var currentY = $event.originalEvent.touches ? $event.originalEvent.touches[0].pageY : $event.pageY;
-            if (currentX > lastX && (currentY - lastY) < 20 ) {
+            
+            if (currentX > lastX && module(currentY - lastY) < 20 ) {
                 $(element).find(".center2, .center").css("left", "0px");
             } else {
                 $(element).find(".center2, .center").css("left", "-240px");
             }
             lastX = currentX;
             lastY = currentY;
-          }
+          //}
         });  
-      }
+      //}
       
     }
   }
