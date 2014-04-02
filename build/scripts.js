9ae3b23f06edd8bd029ad7670c49034ebb71ca6e
Ubuntu <ubuntu@ip-10-234-59-30.us-west-2.compute.internal>
2014-04-02 13:40:56 +0000

    build

--- build/scripts.js
+++ build/scripts.js
@@ -3954,62 +3954,6 @@ pgrModule.config(function(GooglePlusProvider) {
        scopes: SocialConfig.googlePlus.scopes
      });
 });
-(function($) { 
-  $.fn.swipeEvents = function() {
-    return this.each(function() {
-      
-      var startX,
-          startY,
-          $this = $(this);
-      
-      $this.bind('touchstart', touchstart);
-      
-      function touchstart(event) {
-        var touches = event.originalEvent.touches;
-        if (touches && touches.length) {
-          startX = touches[0].pageX;
-          startY = touches[0].pageY;
-          $this.bind('touchmove', touchmove);
-          $this.bind('touchend', touchend);
-        }
-        event.preventDefault();
-      }
-      
-      function touchmove(event) {
-        var touches = event.originalEvent.touches;
-        if (touches && touches.length) {
-          var deltaX = startX - touches[0].pageX;
-          var deltaY = startY - touches[0].pageY;
-          
-          if (deltaX >= 50) {
-            $this.trigger("swipeLeft");
-          }
-          if (deltaX <= -50) {
-            $this.trigger("swipeRight");
-          }
-          if (deltaY >= 50) {
-            $this.trigger("swipeUp");
-          }
-          if (deltaY <= -50) {
-            $this.trigger("swipeDown");
-          }
-          if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
-            $this.unbind('touchmove', touchmove);
-            $this.unbind('touchend', touchend);
-          }
-        }
-        event.preventDefault();
-      }
-      
-      function touchend(event) {
-        $this.unbind('touchmove', touchmove);
-        event.preventDefault();
-      }
-      
-    });
-  };
-})(jQuery);
-
 pgrModule.directive('errSrc', function() {
   return {
     link: function(scope, element, attrs) {
@@ -4376,7 +4320,7 @@ pgrModule.directive('showcrits', function($window) {
             //console.log($event.originalEvent);
             var currentX = $event.originalEvent.touches ? $event.originalEvent.touches[0].pageX : $event.pageX;
             var currentY = $event.originalEvent.touches ? $event.originalEvent.touches[0].pageY : $event.pageY;
-            if (currentX > lastX ) {
+            if (currentX > lastX && (currentY - lastY) < 20 ) {
                 $(element).find(".center2, .center").css("left", "0px");
             } else {
                 $(element).find(".center2, .center").css("left", "-240px");
