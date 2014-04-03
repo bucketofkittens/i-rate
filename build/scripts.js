bbfd5bf359a9509c4d914e3a66b98c2463e21f1f
Ubuntu <ubuntu@ip-10-234-59-30.us-west-2.compute.internal>
2014-04-03 09:30:42 +0000

    build

--- build/scripts.js
+++ build/scripts.js
@@ -4328,14 +4328,12 @@ pgrModule.directive('showcrits', function($window) {
             //console.log($event.originalEvent);
             var currentX = $event.originalEvent.touches ? $event.originalEvent.touches[0].pageX : $event.pageX;
             var currentY = $event.originalEvent.touches ? $event.originalEvent.touches[0].pageY : $event.pageY;
-            console.log(module(currentY - lastY));
-            if((module(currentY - lastY) == 0)) {
-              if (currentX > lastX) {
-                  $(element).find(".center2, .center").css("left", "0px");
-              } else {
-                  $(element).find(".center2, .center").css("left", "-240px");
-              }  
-            }
+            
+            if (currentX > lastX) {
+                $(element).find(".center2, .center").css("left", "0px");
+            } else {
+                $(element).find(".center2, .center").css("left", "-240px");
+            }  
             
             lastX = currentX;
             lastY = currentY;
