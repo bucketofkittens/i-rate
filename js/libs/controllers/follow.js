7dd3783fea15f519e03552a5614e08848dc1bb4a
lyykfi <lyykfi@gmail.com>
2014-04-11 15:11:30 +0400

    add terms link

--- js/libs/controllers/follow.js
+++ js/libs/controllers/follow.js
@@ -6,7 +6,7 @@ function FollowController($scope, $rootScope) {
     $scope.stepWidth = 48;
 
     if($scope.phone) {
-        $scope.max = 4;
+        $scope.max = 7;
         $scope.stepWidth = 38;
     }
 
